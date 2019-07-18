c84c034742e1289a6249504a96d7f2f657f26a54
Zigmata <scafidif@gmail.com>
2019-07-18 14:06:08 -0700

    ReQuest.py - Added channel set and post commands

--- ReQuest/ReQuest/ReQuest.py
+++ ReQuest/ReQuest/ReQuest.py
@@ -1,21 +1,44 @@
 import discord
 
 client = discord.Client()
+# TODO: load server settings from database
 
+# declare global vars
+postChannel = ''
+
+# Print message on login
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
 
+# Add commands here
 @client.event
 async def on_message(message):
+	global postChannel
+
 	if message.author == client.user:
 		return
+	
+	# Command prefix set to r!
+	# TODO: allow changing of command prefix
+	if message.content[:2] == 'r!':
+		args=message.content[2:].split()
+		cmd=args[0]
 
-	if message.content.startswith('$hello'):
+	if cmd == 'hello':
 		await message.channel.send('Hello!')
 
-	if message.content.startswith('$invite'):
-		await message.channel.send('Invite me to your server! https://discordapp.com/api/oauth2/authorize?client_id=601492201704521765&permissions=388160&scope=bot')
+	if cmd == 'invite':
+		await message.channel.send('Invite me to your server! <https://discordapp.com/api/oauth2/authorize?client_id=601492201704521765&permissions=388160&scope=bot>')
+
+	if cmd == 'channel':
+		postChannel = args[1]
+		await message.channel.send('Successfully set quest channel to {0}!'.format(postChannel))
+
+	if cmd == 'post':
+		c = client.get_channel(int(postChannel.strip('<').strip('>').strip('#')))
+		content = args[1]
+		await c.send(content)
 
 f=open('token.txt','r')
 if f.mode == 'r':
