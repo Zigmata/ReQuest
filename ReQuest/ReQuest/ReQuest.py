aa2e903f86cb2ed938ff48cf7fd14ad1e0bb871b
Zigmata <scafidif@gmail.com>
2019-07-18 17:08:11 -0700

    ReQuest.py - Added post command and formatted with reaction

--- ReQuest/ReQuest/ReQuest.py
+++ ReQuest/ReQuest/ReQuest.py
@@ -1,10 +1,13 @@
 import discord
+import textwrap
+import itertools
 
 client = discord.Client()
 # TODO: load server settings from database
 
 # declare global vars
 postChannel = ''
+announceRole = ''
 
 # Print message on login
 @client.event
@@ -15,6 +18,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	global postChannel
+	global announceRole
 
 	if message.author == client.user:
 		return
@@ -25,20 +29,23 @@ async def on_message(message):
 		args=message.content[2:].split()
 		cmd=args[0]
 
-	if cmd == 'hello':
-		await message.channel.send('Hello!')
-
-	if cmd == 'invite':
+	if cmd == 'info':
 		await message.channel.send('Invite me to your server! <https://discordapp.com/api/oauth2/authorize?client_id=601492201704521765&permissions=388160&scope=bot>')
 
 	if cmd == 'channel':
 		postChannel = args[1]
 		await message.channel.send('Successfully set quest channel to {0}!'.format(postChannel))
 
+	if cmd == 'announce':
+		announceRole = args[1]
+		await message.channel.send('Successfully set announcement role to {0}!'.format(announceRole))
+
 	if cmd == 'post':
 		c = client.get_channel(int(postChannel.strip('<').strip('>').strip('#')))
-		content = args[1]
-		await c.send(content)
+		title, levels ,gm, slots, description, role = args[1], args[2], args[3], args[4], args[5], args[6]
+		msg = await c.send(f'{announceRole}\n**NEW QUEST:** {title}\n**Level Range:** {levels}\n**GM:** {gm}\n**Players:**\n**Description:** {description}')
+		emoji = '<:acceptquest:601559094293430282>'
+		await msg.add_reaction(emoji)
 
 f=open('token.txt','r')
 if f.mode == 'r':
