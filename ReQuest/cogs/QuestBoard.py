9470426f71656da59383564b2b73013328c8c71b
Zigmata <scafidif@gmail.com>
2020-05-07 16:32:01 -0700

    questBoard.py - Quest posts now have formatting.

--- ReQuest/cogs/QuestBoard.py
+++ ReQuest/cogs/QuestBoard.py
@@ -9,15 +9,28 @@ class QuestBoard(Cog):
 
     @listener()
     async def on_reaction_add(self, reaction, user):
+        message = reaction.message
+        original = message.content
         if user.bot:
             return
         else:
             await message.edit(content = original+f'\n- <@!{user.id}>')
 
+    @listener()
+    async def on_reaction_remove(self, reaction, user):
+        #message = reaction.message
+        #original = message.content
+        
+        # needs to index a regex of user mention, then remove that substring somehow
+        #if user.bot:
+        #    return
+        #else:
+        #    await message.edit()
+
     @command()
-    async def post(self, ctx):
-        msg = await ctx.send('Successful Test!')
-        #msg = await ctx.send(f'{announceRole}\n**NEW QUEST:** {title}\n**Level Range:** {levels}\n**GM:** {gm}\n**Description:** {description}\n**Players:**')
+    async def post(self, ctx, title, levels, description):
+        gm = f'<@!{ctx.author.id}>'
+        msg = await ctx.send(f'announceRole\n**NEW QUEST:** {title}\n**Level Range:** {levels}\n**GM:** {gm}\n**Description:** {description}\n**Players:**')
         emoji = '<:acceptquest:601559094293430282>'
         await msg.add_reaction(emoji)
         await msg.channel.send('Quest posted!')
