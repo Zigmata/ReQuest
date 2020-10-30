0ca710d647aac08e29c18121eff938ec96b749c8
Zigmata <scafidif@gmail.com>
2020-10-30 12:18:46 -0700

    Added bot construction parameter for privilged gateway intents (breaking change from discord.py 1.5)

--- ReQuest/bot.py
+++ ReQuest/bot.py
@@ -21,10 +21,15 @@ class ReQuest(commands.AutoShardedBot):
     def __init__(self, prefix, **options):
         super(ReQuest, self).__init__(prefix, **options)
 
+# Define privileged gateway intents
+
+intents = discord.Intents.default()
+intents.members = True  # Subscribe to the privileged members intent.
+
 # Define bot and prefix
 # TODO: Implement prefix changes
 pre = config['prefix']
-bot = ReQuest(prefix=pre, case_insensitive = True, activity=discord.Game(name=f'by Post | $help'))
+bot = ReQuest(prefix=pre, case_insensitive = True, intents=intents, activity=discord.Game(name=f'by Post | $help'))
 #bot.remove_command('help') # TODO: Un-comment when custom help commands are implemented.
 bot.config = config
 bot.gdb = connection[config['guildDb']]
