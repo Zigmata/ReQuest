3924b4ec7c936b47e16e8b15159e3fca8e0723aa
Zigmata <scafidif@gmail.com>
2020-12-30 00:20:13 -0800

    Added some logic to char building for initial inventory setup. Buggy as fuck. Can't handle items with more than one word in name.

--- ReQuest/cogs/player.py
+++ ReQuest/cogs/player.py
@@ -4,6 +4,7 @@ import shortuuid
 import itertools
 import bson
 import re
+import time
 
 import pymongo
 from pymongo import MongoClient
@@ -24,7 +25,7 @@ class Player(Cog):
         gdb = bot.gdb
         mdb = bot.mdb
 
-    @commands.group(aliases = ['char'], hidden = True, invoke_without_command = True, case_insensitive = True)
+    @commands.group(aliases = ['char'], invoke_without_command = True, case_insensitive = True)
     async def character(self, ctx, character_name : str = None):
         if ctx.invoked_subcommand is None:
             member_id = ctx.author.id
@@ -137,6 +138,12 @@ class Player(Cog):
         await ctx.send(f'{character_name} registered with ID `{character_id}`!\nDo you wish to set up initial attributes?\n(**Y**)es or (**N**)o')
         reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
         if any(x in reply.content.lower() for x in ['no', 'n']):
+            # Adds the provided character info to the db
+            collection.update_one({'memberId': member_id}, {'$set': {'activeChar': character_id,
+                f'characters.{character_id}': {'name': character_name,
+                'note': character_note, 'registeredDate': date, 'attributes': {'level': None,
+                'experience': char_xp, 'inventory': inventory, 'currency': None}}}}, upsert = True)
+
             await delete_command(reply)
         elif any(x in reply.content.lower() for x in ['y', 'yes']):
             await delete_command(reply)
@@ -149,24 +156,104 @@ class Player(Cog):
                 xp_reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author) # TODO: Check for int
                 char_xp = int(xp_reply.content)
 
-            # Prompt for initial inventory
-            await ctx.send('Import inventory as JSON (**c** to cancel)')
-            inventory_reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
-            if not 'c' in inventory_reply.content.lower():
-                inventory = inventory_reply.content
+            collection.update_one({'memberId': member_id}, {'$set': {'activeChar': character_id,
+                f'characters.{character_id}': {'name': character_name,
+                'note': character_note, 'registeredDate': date, 'attributes': {'level': None,
+                'experience': char_xp, 'inventory': inventory, 'currency': None}}}}, upsert = True)
 
-        # Adds the provided character info to the db
-        collection.update_one({'memberId': member_id}, {'$set': {'activeChar': character_id,
-            f'characters.{character_id}': {'name': character_name,
-            'note': character_note, 'registeredDate': date, 'attributes': {'level': None,
-            'experience': char_xp, 'inventory': inventory, 'currency': None}}}}, upsert = True)
+            reply = True
+            while reply:
+                await ctx.send('Add an item and quantity to initial inventory (**c** to cancel)\nFor example: Torch 3')
+                inventory_reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
+                if not inventory_reply.content.lower() == 'c':
+                    item = inventory_reply.content.split()
+                    name = item[0]
+                    quantity = int(item[1])
+
+                    query = collection.find_one({'memberId': member_id})
+                    current_inventory = query['characters'][character_id]['attributes']['inventory']
+                    if name in current_inventory:
+                        current_quantity = current_inventory[name]
+                        new_quantity = current_quantity + quantity
+                        collection.update_one({'memberId': member_id}, {'$set': {f'characters.{character_id}.attributes.inventory.{name}': new_quantity}}, upsert = True)
+                    else:
+                        collection.update_one({'memberId': member_id}, {'$set': {f'characters.{character_id}.attributes.inventory.{name}': quantity}}, upsert = True)
+                else:
+                    reply = False
+                
+            ## Prompt for initial inventory
+            #await ctx.send('Import inventory as JSON (**c** to cancel)')
+            #inventory_reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
+            #if not 'c' in inventory_reply.content.lower():
+            #    inventory = inventory_reply.content
 
         await ctx.send('Character registration saved!')
 
         await delete_command(ctx.message)
 
+    @character.command(name = 'delete', aliases = ['remove', 'del', 'rem'])
+    async def character_delete(self, ctx, character_name):
+        """
+        Deletes a player character.
+
+        Arguments:
+        [character_name]: The name of the character.
+        """
+        member_id = ctx.author.id
+        collection = mdb['characters']
+        query = collection.find_one({'memberId': member_id})
+
+        ids = []
+        if not query:
+            await ctx.send('You have no registered characters!')
+            await delete_command(ctx.message)
+            return
+        else:
+            for id in query['characters']:
+                ids.append(id)
+
+        name = character_name.lower()
+        matches = []
+        for id in ids:
+            char = query['characters'][id]
+            if name in char['name'].lower():
+                matches.append(id)
+
+        if not matches:
+            await ctx.send('No characters found with that name!')
+            await delete_command(ctx.message)
+            return
+        elif len(matches) == 1:
+            char = query['characters'][matches[0]]
+            collection.update_one({'memberId': member_id}, {'$pull': {'characters': char}})
+            # TODO: Set active character to first in list
+        elif len(matches) > 1:
+            content = ''
+            for i in range(len(matches)):
+                content += '{}: {} ({})\n'.format(i+1, query['characters'][matches[i]]['name'], query['characters'][matches[i]]['note'])
+
+            match_embed = discord.Embed(title = "Your query returned more than one result!", type = 'rich',
+                description = content)
+
+            match_msg = await ctx.send(embed=match_embed)
+
+            reply = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
+            if int(reply.content) > len(matches):
+                await delete_command(ctx.message)
+                await delete_command(match_msg)
+                await delete_command(reply)
+                await ctx.send('Selection is outside the list of options.')
+                return
+            else:
+                await delete_command(match_msg)
+                await delete_command(reply)
+                selection = query['characters'][matches[int(reply.content)-1]]
+                collection.update_one({'memberId': member_id}, {'$pull': {'characters': matches[int(reply.content)-1]}})
+                # TODO: Set active character to first in list
+
     @character.command(hidden = True)
     async def give(self, ctx, item_name, quantity: int):
+        # TODO: Testing only / refactor for GM use later
         member_id = ctx.author.id
         guild_id = ctx.message.guild.id
         collection = mdb['characters']
@@ -182,36 +269,58 @@ class Player(Cog):
         else:
             collection.update_one({'memberId': member_id}, {'$set': {f'characters.{active_character}.attributes.inventory.{item_name}': quantity}}, upsert = True)
 
-        await ctx.send(f'{quantity} of {item_name} added to inventory!')
+        response = await ctx.send(f'{quantity} of {item_name} added to inventory!')
+
+        time.sleep(1)
 
         await delete_command(ctx.message)
+        await response.delete()
 
-    @commands.group(name = 'xp', invoke_without_subcommand = True, case_insensitive = True)
-    async def cxp(self, ctx):
-        if ctx.invoked_subcommand is None:
-            member_id = ctx.author.id
-            guild_id = ctx.message.guild.id
-            collection = mdb['characters']
+    @command()
+    async def xp(self, ctx, value : int = None):
+        # TODO: error handling for non integer values given
+        member_id = ctx.author.id
+        guild_id = ctx.message.guild.id
+        collection = mdb['characters']
 
-            # Load the author's characters
-            query = collection.find_one({'memberId': member_id})
-            if not query: # If none exist, output the error
-                await ctx.send('You have no registered characters!')
-                await delete_command(ctx.message)
-                return
+        # Load the author's characters
+        query = collection.find_one({'memberId': member_id})
+        if not query: # If none exist, output the error
+            await ctx.send('You have no registered characters!')
+            await delete_command(ctx.message)
+            return
 
-            # Otherwise, proceed to query the active character and retrieve its xp
-            active_id = query['activeChar']
-            active_character = query['characters'][active_id]
-            name = active_character['name']
-            xp = active_character['attributes']['experience']
+        # Otherwise, proceed to query the active character and retrieve its xp
+        active_character = query['activeChar']
+        char = query['characters'][active_character]
+        name = char['name']
+        xp = char['attributes']['experience']
+
+        # If no argument was provided, output the character's current experience
+        if value == None:
             if xp:
-                post_embed = discord.Embed(title=name+'\'s Experience', type='rich', description=xp)
+                post_embed = discord.Embed(title=f'{name}\'s Experience', type='rich', description=f'Total Experience: {xp}')
                 await ctx.send(embed=post_embed)
             else:
-                await ctx.send(name+' is rather inexperienced! Did you forget to add some?')
+                await ctx.send(f'{name} is rather inexperienced! Did you forget to add some?')
+        else: # Otherwise, adjust the xp based on the value given.
+            if xp:
+                xp = xp + value
+            else:
+                xp = value
 
-            await delete_command(ctx.message)
+            # Update the db
+            collection.update_one({'memberId': member_id}, {'$set': {f'characters.{active_character}.attributes.experience': xp}}, upsert = True)
+
+            # Dynamic feedback based on the operation performed
+            function = 'adds'
+            if value < 0:
+                function = 'removes'
+            absolute = abs(value)
+            post_embed = discord.Embed(title=f'{name} {function} {absolute} experience points!', type='rich', description=f'Total Experience: {xp}')
+            await ctx.send(embed=post_embed)
+
+        await delete_command(ctx.message)
 
 def setup(bot):
     bot.add_cog(Player(bot))
