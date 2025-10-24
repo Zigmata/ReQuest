c25e30c28982f52cde2504da89db0199db0c3506
Zigmata <37376040+Zigmata@users.noreply.github.com>
2025-10-24 10:26:08 -0700

    Adjust TextStyle reference

--- ReQuest/ui/inputs.py
+++ ReQuest/ui/inputs.py
@@ -5,7 +5,7 @@ class AddCurrencyDenominationTextInput(discord.ui.TextInput):
     def __init__(self, input_type, placeholder):
         super().__init__(
             label=input_type,
-            style=discord.TextStyle.short,
+            style=discord.TextStyle['short'],
             placeholder=placeholder,
             custom_id=f'denomination_{input_type.lower()}_text_input',
             required=True
